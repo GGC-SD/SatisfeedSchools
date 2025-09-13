76ab87f2460191d78978d7cfd90d4ba4078fbcce
Dylan <dlong12@ggc.edu>
2025-09-13 18:57:01 -0400

    feat: add and styles school map buttons

--- code/src/app/dashboard-schools/components/dashboard-schools-map.tsx
+++ code/src/app/dashboard-schools/components/dashboard-schools-map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useRef } from "react";
 import maplibregl, { Map as MLMap, LngLatLike } from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
+// import "maplibre-gl/dist/maplibre-gl.css";
 
 type Props = {
   className?: string;
@@ -29,6 +29,30 @@ export default function DashboardSchoolsMap({
       zoom,
     });
 
+    const ResetButton = class implements maplibregl.IControl {
+      private container!: HTMLDivElement;
+
+      onAdd() {
+        const btn = document.createElement("button");
+        btn.type = "button";
+        btn.textContent = "Reset";
+        btn.className = "maplibregl-ctrl my-reset-btn"; // styling hook
+
+        const wrapper = document.createElement("div");
+        wrapper.className = "maplibregl-ctrl my-reset-wrapper";
+        wrapper.appendChild(btn);
+
+        this.container = wrapper;
+        return wrapper;
+      }
+
+      onRemove() {
+        this.container.remove();
+      }
+    };
+
+    map.addControl(new ResetButton(), "top-right");
+
     // zoom buttons
     map.addControl(
       new maplibregl.NavigationControl({ visualizePitch: true }),
@@ -52,5 +76,5 @@ export default function DashboardSchoolsMap({
     };
   }, [center, zoom]);
 
-  return <div ref={containerRef} className={className} />;
+  return <div ref={containerRef} className={`${className} school-map`} />;
 }
--- code/src/app/dashboard-schools/components/school-display.tsx
+++ code/src/app/dashboard-schools/components/school-display.tsx
@@ -4,21 +4,19 @@ import DataCard from "./DataCard";
 import DashboardSchoolsMap from "./dashboard-schools-map";
 
 export default function SchoolDisplay() {
-
-    return(
-        <div className="p-4">
-            <div className="flex flex-row gap-4">
-                <div className="w-3/5">
-                    <div className="h-full">
-                        <DashboardSchoolsMap className="w-full h-full"></DashboardSchoolsMap>
-                    </div>
-                </div>
-                <div className="w-2/5 flex flex-col gap-4">
-                    <CountyZIPDropdown />
-                    <DataCard />
-                </div>
-            </div>
+  return (
+    <div className="p-4">
+      <div className="flex flex-row gap-4">
+        <div className="w-3/5">
+          <div className="h-full">
+            <DashboardSchoolsMap className="w-full h-full"></DashboardSchoolsMap>
+          </div>
         </div>
-    );
+        <div className="w-2/5 flex flex-col gap-4">
+          <CountyZIPDropdown />
+          <DataCard />
+        </div>
+      </div>
+    </div>
+  );
 }
-
