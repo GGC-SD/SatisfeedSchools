4c0a7e01f49e237055a5f62b3206f57be5595830
Sam <samuelgkeller@gmail.com>
2025-09-13 15:56:53 -0400

    feat: Add map to school display

--- code/src/app/dashboard-schools/components/school-display.tsx
+++ code/src/app/dashboard-schools/components/school-display.tsx
@@ -7,13 +7,13 @@ export default function SchoolDisplay() {
 
     return(
         <div className="p-4">
-            <div className="flex">
-                <div className="w-2/3">
+            <div className="flex flex-row gap-4">
+                <div className="w-3/5">
                     <div className="h-full">
-                        <DashboardSchoolsMap className="w-[65%] h-full"></DashboardSchoolsMap>
+                        <DashboardSchoolsMap className="w-full h-full"></DashboardSchoolsMap>
                     </div>
                 </div>
-                <div className="w-1/3 flex flex-col gap-4">
+                <div className="w-2/5 flex flex-col gap-4">
                     <CountyZIPDropdown />
                     <DataCard />
                 </div>
