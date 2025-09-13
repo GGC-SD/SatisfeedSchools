5e47e9df4ed954ce115ec55252a237cb6c6778bc
Dylan <dlong12@ggc.edu>
2025-09-13 15:29:32 -0400

    feat: add map

---
+++ code/src/app/dashboard-schools/components/dashboard-schools-map.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useEffect, useRef } from "react";
+import maplibregl, { Map as MLMap, LngLatLike } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+type Props = {
+  className?: string;
+  center?: LngLatLike; // starting position [lng, lat]
+  zoom?: number;
+};
+
+export default function DashboardSchoolsMap({
+  className = "w-full h-full",
+  center = [-84.07, 33.95], // coords of Gwinnett County
+  zoom = 11,
+}: Props) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<MLMap | null>(null);
+
+  useEffect(() => {
+    if (!containerRef.current || mapRef.current) return;
+
+    // from env.local
+    const apiKey = process.env.NEXT_PUBLIC_MAPTILER_KEY!;
+    const map = new maplibregl.Map({
+      container: containerRef.current,
+      style: `https://api.maptiler.com/maps/topo-v2/style.json?key=${apiKey}`,
+      center,
+      zoom,
+    });
+
+    // zoom buttons
+    map.addControl(
+      new maplibregl.NavigationControl({ visualizePitch: true }),
+      "top-right"
+    );
+
+    // Scale bar` (i.e. 10 miles)
+    map.addControl(
+      new maplibregl.ScaleControl({ unit: "imperial" }),
+      "bottom-left"
+    );
+
+    const ro = new ResizeObserver(() => map.resize());
+    ro.observe(containerRef.current);
+
+    mapRef.current = map;
+    return () => {
+      ro.disconnect();
+      map.remove();
+      mapRef.current = null;
+    };
+  }, [center, zoom]);
+
+  return <div ref={containerRef} className={className} />;
+}
--- code/src/app/dashboard-schools/components/school-display.tsx
+++ code/src/app/dashboard-schools/components/school-display.tsx
@@ -1,10 +1,12 @@
 "use client";
+import DashboardSchoolsMap from "./dashboard-schools-map";
 
 export default function SchoolDisplay() {
-
-    return(
-        <div className="p-4">
-            This is the school display
-        </div>
-    );
+  return (
+    <div className="h-full p-4">
+      <div className="h-full">
+        <DashboardSchoolsMap className="w-[65%] h-full"></DashboardSchoolsMap>
+      </div>
+    </div>
+  );
 }
