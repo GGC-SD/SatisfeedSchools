609449dc44b9cd93bb01b04a93ad75ec7a3e8859
eawere <eawere@ggc.edu>
2025-09-13 12:45:36 -0400

    feat: Add DataCard.tsx

---
+++ code/src/components/DataCard.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+//import use state for functionality
+
+/**
+ * This component creates a card that displays data depending on the County and ZIP code selected.
+ * @returns A card that will contain relevant data to the County and ZIP code selected in the
+ * dropdown menus
+ */
+export default function DataCard() {
+
+    return (
+        <div className="min-w-[150px] min-h-[400px] card child-component-borders">
+        </div>
+    );
+
+}
